6f89e69c222e374f2ea6253c848c70785e7fb440
Erlend Dahl <erlend.dahl@sintef.no>
2021-10-25 10:57:33 +0200

    Added shape exporting.

--- RoadNetworkRouting/RoadNetworkRouter.cs
+++ RoadNetworkRouting/RoadNetworkRouter.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using DotSpatial.Data;
+using DotSpatial.Topology;
 using Extensions.IEnumerableExtensions;
 using no.sintef.SpeedModule.Geometry;
 using no.sintef.SpeedModule.Geometry.SimpleStructures;
@@ -257,6 +259,96 @@ namespace RoadNetworkRouting
                     v.VertexGroup = gid;
                 else
                     v.VertexGroup = -1;
+        }
+
+        public void ExportNodes(string shpPath)
+        {
+            var shp = new FeatureSet(FeatureType.Point);
+
+            var table = shp.DataTable;
+            table.Columns.Add("Id", typeof(int));
+            table.Columns.Add("Edges", typeof(int));
+            table.AcceptChanges();
+
+            foreach (var c in Vertices.Values)
+            {
+                var feature = shp.AddFeature(new Point(new Coordinate(c.X, c.Y)));
+                feature.DataRow["Id"] = c.Id;
+                feature.DataRow["Edges"] = c.Edges;
+            }
+
+            shp.SaveAs(shpPath, true);
+        }
+
+        public void ExportLinks(string shpPath)
+        {
+            var shp = new FeatureSet(FeatureType.Point);
+
+            var table = shp.DataTable;
+            table.Columns.Add("Reference", typeof(string));
+            table.Columns.Add("Id", typeof(int));
+            table.Columns.Add("FromNode", typeof(int));
+            table.Columns.Add("ToNode", typeof(int));
+            table.Columns.Add("FromTolr.", typeof(int));
+            table.Columns.Add("ToTolr.", typeof(int));
+            table.Columns.Add("Speed", typeof(int));
+            table.Columns.Add("Speed rev.", typeof(int));
+            table.Columns.Add("Cost", typeof(double));
+            table.Columns.Add("Cost rev.", typeof(double));
+            table.AcceptChanges();
+
+            foreach (var c in Links.Values)
+            {
+                var feature = shp.AddFeature(new LineString(c.Geometry.Points.Select(p => new Coordinate(p.X, p.Y))));
+                feature.DataRow["FromNode"] = c.FromNodeId;
+                feature.DataRow["ToNode"] = c.ToNodeId;
+                feature.DataRow["Id"] = c.LinkId;
+                feature.DataRow["Reference"] = c.Reference;
+                feature.DataRow["FromTolr."] = c.FromNodeConnectionTolerance;
+                feature.DataRow["ToTolr."] = c.ToNodeConnectionTolerance;
+                feature.DataRow["Cost"] = c.Cost;
+                feature.DataRow["Cost rev."] = c.ReverseCost;
+                feature.DataRow["Speed"] = c.SpeedLimit;
+                feature.DataRow["Speed rev."] = c.SpeedLimitReversed;
+            }
+
+            shp.SaveAs(shpPath, true);
+        }
+
+        public void ExportNodeConnections(string shpPath)
+        {
+            var shp = new FeatureSet(FeatureType.Point);
+
+            var table = shp.DataTable;
+            table.Columns.Add("LinkId", typeof(int));
+            table.Columns.Add("NodeId", typeof(int));
+            table.Columns.Add("FromOrTo", typeof(char));
+            table.Columns.Add("Distance", typeof(double));
+            table.Columns.Add("Tolerance", typeof(int));
+            table.AcceptChanges();
+
+            foreach (var c in Links.Values)
+            {
+                var node = Vertices[c.FromNodeId];
+                var coordinate = c.Geometry.Points.First();
+                var feature = shp.AddFeature(new LineString(new[] { new Coordinate(node.X, node.Y), new Coordinate(coordinate.X, coordinate.Y) }));
+                feature.DataRow["LinkId"] = c.LinkId;
+                feature.DataRow["NodeId"] = node.Id;
+                feature.DataRow["FromOrTo"] = 0;
+                feature.DataRow["Distance"] = coordinate.DistanceTo2D(node.X, node.Y);
+                feature.DataRow["Tolerance"] = c.FromNodeConnectionTolerance;
+
+                node = Vertices[c.ToNodeId];
+                coordinate = c.Geometry.Points.Last();
+                feature = shp.AddFeature(new LineString(new[] { new Coordinate(node.X, node.Y), new Coordinate(coordinate.X, coordinate.Y) }));
+                feature.DataRow["LinkId"] = c.LinkId;
+                feature.DataRow["NodeId"] = node.Id;
+                feature.DataRow["FromOrTo"] = 2;
+                feature.DataRow["Distance"] = coordinate.DistanceTo2D(node.X, node.Y);
+                feature.DataRow["Tolerance"] = c.ToNodeConnectionTolerance;
+            }
+
+            shp.SaveAs(shpPath, true);
         }
     }
 }

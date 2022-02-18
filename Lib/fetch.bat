2b9bcf927d221109fd47bc425587e7cdf01e995e
Erlend Dahl <erlend.dahl@sintef.no>
2022-02-18 16:00:01 +0100

    Use local dependencies.

---
+++ Lib/fetch.bat
@@ -0,0 +1,7 @@
+copy "D:\Nextcloud\Mine program\C#\Extensions\ByteUtilities\bin\Release\ByteUtilities.dll" .
+copy "D:\Nextcloud\Mine program\C#\Extensions\ConsoleUtilities\bin\Release\ConsoleUtilities.dll" .
+copy "D:\Nextcloud\Mine program\C#\Extensions\Extensions\bin\Release\netstandard2.0\Extensions.dll" .
+copy "D:\Nextcloud\Mine program\C#\Extensions\DataflowUtilities\bin\Release\DataflowUtilities.dll" .
+copy "C:\Code\RTM\rtm_tester\Gdb2Xml\Gdb2Xml\Gdb2Xml\bin\Release\Gdb2Xml.exe" .
+copy "C:\Code\EnergyModule\Fartsmodell\bin\Release\SpeedModule.dll" .
+pause

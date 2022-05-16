c3d9d9deb61a0494e51e74646e355a16ac489163
Erlend Dahl <erlend.dahl@sintef.no>
2022-05-16 14:49:09 +0200

    Extracted class to own file.

--- Routing/Dijkstra.cs
+++ Routing/Dijkstra.cs
@@ -1,52 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace Routing
 {
-    public class DijkstraResult
-    {
-        private readonly Graph _graph;
-        private readonly Dictionary<int, VertexData> _dynamicData;
-        private readonly Stopwatch _stopwatch;
-
-        public VertexData Source { get; set; }
-        public VertexData Target { get; private set; }
-        public TimeSpan ElapsedTime { get; set; }
-
-        public int Tries { get; set; }
-
-        public DijkstraResult(Graph graph)
-        {
-            _graph = graph; 
-            _dynamicData = new Dictionary<int, VertexData>();
-            _stopwatch = new Stopwatch();
-            _stopwatch.Start();
-        }
-
-        public VertexData GetVertexData(int id)
-        {
-            if (_dynamicData.TryGetValue(id, out var vd)) return vd;
-            vd = new VertexData(_graph.Vertices[id]);
-            _dynamicData.Add(id, vd);
-            return vd;
-        }
-
-        public DijkstraResult Finish(VertexData target = null)
-        {
-            _stopwatch.Stop();
-            ElapsedTime = _stopwatch.Elapsed;
-            Target = target;
-            return this;
-        }
-
-        public bool HasVisitedVertex(int id)
-        {
-            return _dynamicData.ContainsKey(id);
-        }
-    }
-
     public class Dijkstra
     {
         public static DijkstraResult GetShortestPath(Graph graph, int sourceVertexId, int targetVertexId)

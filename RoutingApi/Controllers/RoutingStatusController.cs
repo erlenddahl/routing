daf1fbe4e5673a34cc2952efa19cbd745e0c7e3c
Erlend Dahl <erlend.dahl@sintef.no>
2023-05-03 20:50:23 +0200

    Added a status controller for easy stats retrieval.

---
+++ RoutingApi/Controllers/RoutingStatusController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using RoutingApi.Geometry;
+using RoutingApi.Helpers;
+
+namespace RoutingApi.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors]
+    public class RoutingStatusController : Controller
+    {
+        [HttpGet]
+        public object Get()
+        {
+            return new
+            {
+                Api = "Routing",
+                Version = "3",
+                LocalDijkstraRoutingService.LoadedAt,
+                LocalDijkstraRoutingService.GlobalTimings,
+                LocalDijkstraRoutingService.TotalRequests,
+                LocalDijkstraRoutingService.TotalWaypoints
+            };
+        }
+    }
+}

f6064a2094550921b98796921fe8d4e60d3f67c5
Erlend Dahl <erlend.dahl@sintef.no>
2023-05-04 22:57:04 +0200

    Created nearby bounds cache and unit tests.

---
+++ RoadNetworkRouting.Tests/NearbyBoundsCacheTests/CreationTests.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnergyModule.Geometry.SimpleStructures;
+using RoadNetworkRouting.Utils;
+
+namespace RoadNetworkRouting.Tests.NearbyBoundsCacheTests
+{
+    [TestClass]
+    public class CreationTests
+    {
+        [TestMethod]
+        public void SingleItemWithinSingleCell()
+        {
+            // This item is strictly within the 50-100, 50-100 cell.
+            var bounds = new[] { new BoundingBox2D(55, 65, 55, 65) };
+            var cache = NearbyBoundsCache<BoundingBox2D>.FromBounds(bounds, p => p, 50);
+
+            // Check that there are no items in the cells surrounding this item.
+            Assert.AreEqual(0, cache.GetItemsInCell(5, 5).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(55, 5).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(105, 5).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(5, 55).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(105, 55).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(5, 105).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(55, 105).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(105, 105).Count());
+
+            // Check that there is a single item in the middle cell
+            Assert.AreEqual(1, cache.GetItemsInCell(55, 55).Count());
+        }
+
+        [TestMethod]
+        public void SingleItemThatAlmostFillsSingleCell()
+        {
+            // This item is strictly within the 50-100, 50-100 cell.
+            var bounds = new[] { new BoundingBox2D(51, 99, 51, 99) };
+            var cache = NearbyBoundsCache<BoundingBox2D>.FromBounds(bounds, p => p, 50);
+
+            // Check that there are no items in the cells surrounding this item.
+            Assert.AreEqual(0, cache.GetItemsInCell(5, 5).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(55, 5).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(105, 5).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(5, 55).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(105, 55).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(5, 105).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(55, 105).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(105, 105).Count());
+
+            // Check that there is a single item in the middle cell
+            Assert.AreEqual(1, cache.GetItemsInCell(55, 55).Count());
+        }
+
+        [TestMethod]
+        public void SingleItemThatFillsSingleCell()
+        {
+            // This item completely filles the 50_50 cell. Since it is on the border to all the neighbouring cells, it should be included there as well.
+            var bounds = new[] { new BoundingBox2D(50, 100, 50, 100) };
+            var cache = NearbyBoundsCache<BoundingBox2D>.FromBounds(bounds, p => p, 50);
+
+            // Check that there is a single item in all cells surrounding this cell
+            Assert.AreEqual(1, cache.GetItemsInCell(5, 5).Count());
+            Assert.AreEqual(1, cache.GetItemsInCell(55, 5).Count());
+            Assert.AreEqual(1, cache.GetItemsInCell(105, 5).Count());
+            Assert.AreEqual(1, cache.GetItemsInCell(5, 55).Count());
+            Assert.AreEqual(1, cache.GetItemsInCell(55, 55).Count());
+            Assert.AreEqual(1, cache.GetItemsInCell(105, 55).Count());
+            Assert.AreEqual(1, cache.GetItemsInCell(5, 105).Count());
+            Assert.AreEqual(1, cache.GetItemsInCell(55, 105).Count());
+            Assert.AreEqual(1, cache.GetItemsInCell(105, 105).Count());
+        }
+
+        [TestMethod]
+        public void MultipleItemsWithinSingleCell()
+        {
+            // This item is strictly within the 50-100, 50-100 cell.
+            var bounds = new[]
+            {
+                new BoundingBox2D(55, 65, 51, 98),
+                new BoundingBox2D(67, 75, 65, 69),
+                new BoundingBox2D(53, 55, 55, 85)
+            };
+            var cache = NearbyBoundsCache<BoundingBox2D>.FromBounds(bounds, p => p, 50);
+
+            // Check that there are no items in the cells surrounding this item.
+            Assert.AreEqual(0, cache.GetItemsInCell(5, 5).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(55, 5).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(105, 5).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(5, 55).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(105, 55).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(5, 105).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(55, 105).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(105, 105).Count());
+
+            // Check that there is a single item in the middle cell
+            Assert.AreEqual(3, cache.GetItemsInCell(55, 55).Count());
+        }
+
+        [TestMethod]
+        public void SingleItemOverlappingTwoCells()
+        {
+            // This item spans two cells; 50_50 and 100_50
+            var bounds = new[] { new BoundingBox2D(58, 137, 60, 70) };
+            var cache = NearbyBoundsCache<BoundingBox2D>.FromBounds(bounds, p => p, 50);
+
+            // Check that there are no items in the cells surrounding this item.
+            Assert.AreEqual(0, cache.GetItemsInCell(5, 5).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(55, 5).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(105, 5).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(155, 5).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(5, 55).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(155, 55).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(5, 105).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(55, 105).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(105, 105).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(155, 105).Count());
+
+            // Check that there is a single item in the two middle cells
+            Assert.AreEqual(1, cache.GetItemsInCell(55, 55).Count());
+            Assert.AreEqual(1, cache.GetItemsInCell(105, 55).Count());
+        }
+
+        [TestMethod]
+        public void SingleItemOverlappingTwoCellsAroundOrigin()
+        {
+            // This item spans two cells; -50_50 and 50_50
+            var bounds = new[] { new BoundingBox2D(-38, 37, 60, 70) };
+            var cache = NearbyBoundsCache<BoundingBox2D>.FromBounds(bounds, p => p, 50);
+
+            // Check that there are no items in the cells surrounding this item.
+            Assert.AreEqual(0, cache.GetItemsInCell(-95, 5).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(-45, 5).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(5, 5).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(55, 5).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(105, 5).Count());
+
+            Assert.AreEqual(0, cache.GetItemsInCell(-95, 55).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(55, 55).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(105, 55).Count());
+
+            Assert.AreEqual(0, cache.GetItemsInCell(-95,105).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(-45, 105).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(5, 105).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(55, 105).Count());
+            Assert.AreEqual(0, cache.GetItemsInCell(105, 105).Count());
+
+            // Check that there is a single item in the two middle cells
+            Assert.AreEqual(1, cache.GetItemsInCell(-45, 55).Count());
+            Assert.AreEqual(1, cache.GetItemsInCell(5, 55).Count());
+        }
+    }
+}
---
+++ RoadNetworkRouting/Utils/NearbyBoundsCache.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnergyModule.Geometry.SimpleStructures;
+
+namespace RoadNetworkRouting.Utils
+{
+    public class NearbyBoundsCache<T>
+    {
+        private readonly Dictionary<string, List<(T Item, BoundingBox2D Bounds)>> _nearbyLookup = new();
+        public int CellSize { get; }
+
+        private NearbyBoundsCache(int cellSize)
+        {
+            CellSize = cellSize;
+        }
+
+        public static NearbyBoundsCache<T> FromBounds(IEnumerable<T> items, Func<T, BoundingBox2D> bounds, int cellSize)
+        {
+            var cache = new NearbyBoundsCache<T>(cellSize);
+
+            foreach (var item in items)
+            {
+                var b = bounds(item);
+
+                var minX = (int)(b.Xmin / cellSize) * cellSize;
+                if (b.Xmin - minX == 0) minX -= cellSize;
+
+                var maxX = (int)(b.Xmax / cellSize) * cellSize;
+                if (b.Xmax - maxX == 0) maxX += cellSize;
+
+                var minY = (int)(b.Ymin / cellSize) * cellSize;
+                if (b.Ymin - minY == 0) minY -= cellSize;
+
+                var maxY = (int)(b.Ymax / cellSize) * cellSize;
+                if (b.Ymax - maxY == 0) maxY += cellSize;
+
+                for (var x = minX; x <= maxX; x += cellSize)
+                for (var y = minY; y <= maxY; y += cellSize)
+                {
+                    var key = cache.GetNearbyKey(x, y);
+                    if (cache._nearbyLookup.TryGetValue(key, out var list))
+                        list.Add((item, b));
+                    else
+                        cache._nearbyLookup.Add(key, new List<(T, BoundingBox2D)>() { (item, b) });
+                }
+            }
+
+            return cache;
+        }
+
+        public string GetNearbyKey(Point3D point)
+        {
+            return GetNearbyKey(point.X, point.Y);
+        }
+
+        public string GetNearbyKey(double x, double y)
+        {
+            return ((int)(x / CellSize) * CellSize) + "_" + ((int)(y / CellSize) * CellSize);
+        }
+
+        public IEnumerable<T> GetNearbyItems(Point3D point, int searchRadius = 0)
+        {
+            return GetNearbyItems(point.X, point.Y, searchRadius);
+        }
+
+        public IEnumerable<T> GetNearbyItems(double x, double y, int searchRadius = 0)
+        {
+            var r = (int)Math.Ceiling(searchRadius / (double)CellSize) * CellSize;
+            for (var xi = x - r; xi <= x + r; xi += CellSize)
+            for (var yi = y - r; yi <= y + r; yi += CellSize)
+            {
+                var key = GetNearbyKey(x, y);
+                if (_nearbyLookup.TryGetValue(key, out var items))
+                    foreach (var item in items)
+                        if (item.Bounds.Contains(x, y, searchRadius))
+                            yield return item.Item;
+            }
+        }
+
+        public IEnumerable<T> GetItemsInCell(Point3D point)
+        {
+            return GetNearbyItems(point.X, point.Y);
+        }
+
+        public IEnumerable<T> GetItemsInCell(double x, double y)
+        {
+            var key = GetNearbyKey(x, y);
+            if (_nearbyLookup.TryGetValue(key, out var items))
+                foreach (var item in items)
+                    yield return item.Item;
+        }
+    }
+}
